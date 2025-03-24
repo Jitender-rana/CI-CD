fa5111c10bc502ef587ad1d3869bcd6eda7fe746
Jitender-rana <119347955+Jitender-rana@users.noreply.github.com>
2025-03-24 13:18:27 +0530

    fixed

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,17 @@ import express from "express";
 const app=express();
 const port:number=3000;
 
+app.use(express.json());
+
+app.post("/*",(req,res)=>{
+    
+})
+
 app.get("/*",(req,res)=>{
     res.json({
         message: "Iam jitender and iam Learning ci/cd",
-        anothermessage: "hahahahahah"
+        anothermessage: "hahahahahah",
+        chnaged: false,
     })
 })
 app.listen(port,()=>{
